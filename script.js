263b49d8eb2531487722b45f7b18d9347b1c42d5
Catalin Tugui <katamaxx23@gmail.com>
2021-02-26 00:05:02 +0200

    app files

---
+++ script.js
@@ -0,0 +1,74 @@
+let count = 10;
+const apiKey = 'RelRxiVGZavQa10S5cfXC3Sihub5-deiL_YK4wf_V0A';
+let apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+let initialLoad = true;
+apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+function imageLoaded(){
+    imagesLoaded ++;
+    if(imagesLoaded === totalImages) {
+        ready = true;
+        loader.hidden = true;
+        initialLoad = false;
+        count = 30;
+    }
+}
+
+let photosArray = [];
+
+function setAttributes (element, attributes) {
+    for(const key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+}
+
+function displayPhotos(){
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
+    photosArray.forEach((photo) => {
+        const item = document.createElement('a');
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank'
+        })
+        const img = document.createElement('img');
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description
+        })
+        img.addEventListener('load', imageLoaded);
+        item.appendChild(img);
+        imageContainer.appendChild(item);
+    });
+}
+
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+        photosArray = await response.json();
+        displayPhotos();
+    } catch(error) {
+        //error
+    }
+}
+
+//SCROLLING
+
+window.addEventListener('scroll', () => {
+    if(window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready ) {
+        ready = false;
+        getPhotos();
+    }
+})
+
+getPhotos();
+
+
+
